416b96d8217b0675e86976f50a3d914f4bb4898e
LaM0uette <lam0uette.dev@gmail.com>
2022-05-16 15:56:32 +0200

    ajout du placement des pions

--- MorPyon.py
+++ MorPyon.py
@@ -3,8 +3,6 @@ import time
 from enum import Enum
 from termcolor import colored
 
-SIZE_GAME = 3
-
 
 class Player(Enum):
     Player1 = 1
@@ -14,16 +12,17 @@ class Player(Enum):
 class NewGame:
     def __init__(self):
         self.run = True
+        self.size_game = 3
         self.player_turn = Player.Player1
         self.matrix_game = self.gen_matrice()
 
     def gen_matrice(self):
         lst = []
 
-        for row in range(SIZE_GAME):
+        for row in range(self.size_game):
             lst.append([])
 
-            for col in range(SIZE_GAME):
+            for col in range(self.size_game):
                 lst[row].append(0)
 
         return lst
@@ -33,18 +32,52 @@ class NewGame:
 
         print(f"Tour: {self.player_turn.name}" if self.run else '')
 
-        for i in range(SIZE_GAME):
+        for i in range(self.size_game):
             print(f"""                   {''.join(f"[{colored('O', 'yellow') if x == 1 else colored('X', 'red') if x == 2 else colored('O', 'green', attrs=['bold']) if x == 3 else colored('X', 'green', attrs=['bold']) if x == 6 else ' '}]" for x in self.matrix_game[i])}""")
 
         print("")
+        
+    def player_play(self, num_played):
+        if not num_played.isdigit():
+            print("Valeur incorect !")
+            time.sleep(0.5)
+            return
+        if not 0 < int(num_played) < self.size_game**2 + 1:
+            print("Valeur incorect !")
+            time.sleep(0.5)
+            return
+
+        num = int(num_played) - 1
+
+        if 0 <= num <= 2:
+            row = num
+            col = 0
+        elif 3 <= num <= 5:
+            row = num-3
+            col = 1
+        else:
+            row = num - 6
+            col = 2
+
+        if self.matrix_game[col][row] == 0:
+            self.matrix_game[col][row] = self.player_turn.value
+        else:
+            print("Impossible de jouer ici !")
+            time.sleep(1)
+            return
+
+        # self.check_win()
+        self.check_end()
+
+        self.player_turn = Player.Player2 if self.player_turn == Player.Player1 else Player.Player1
 
     def start(self):
-        os.system(f"mode con: cols={38} lines={7}")
+        os.system(f"mode con: cols={38+(self.size_game**2)} lines={4+self.size_game}")
 
         while self.run:
             self.draw_game()
 
-            input_player = input(f"Entrez le numéro d'une colonne (1 - {SIZE_GAME**2}) : ")
-            # self.add_token(token_played=input_player)
+            input_player = input(f"Entrez le numéro d'une colonne (1 - {self.size_game**2}) : ")
+            self.player_play(num_played=input_player)
 
 NewGame().start()
---
+++ start.bat
@@ -0,0 +1,3 @@
+@Echo off
+
+py MorPyon.py
