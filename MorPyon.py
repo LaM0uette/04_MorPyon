b5bbfa9b8a4da700b51b3d0b84a9ce57393defd6
LaM0uette <lam0uette.dev@gmail.com>
2022-05-16 16:08:20 +0200

    coup possibles

--- MorPyon.py
+++ MorPyon.py
@@ -15,6 +15,16 @@ class NewGame:
         self.size_game = 3
         self.player_turn = Player.Player1
         self.matrix_game = self.gen_matrice()
+        self.matrix_win = [
+            [[0, 0], [0, 1], [0, 2]],
+            [[1, 0], [1, 1], [1, 2]],
+            [[2, 0], [2, 1], [2, 2]],
+            [[0, 0], [1, 0], [2, 0]],
+            [[0, 1], [1, 1], [2, 1]],
+            [[0, 2], [1, 2], [2, 2]],
+            [[0, 0], [1, 1], [2, 2]],
+            [[2, 2], [1, 1], [0, 0]],
+        ]
 
     def gen_matrice(self):
         lst = []
@@ -66,10 +76,24 @@ class NewGame:
             time.sleep(1)
             return
 
-        # self.check_win()
+        self.check_win()
         self.check_end()
 
         self.player_turn = Player.Player2 if self.player_turn == Player.Player1 else Player.Player1
+
+    def check_win(self):
+        def check(result):
+            if f"{self.player_turn.value}"*self.size_game in result:
+
+                for i in range(self.size_game):
+                    self.matrix_game[indexWin[i][0]][indexWin[i][1]] = self.player_turn.value*3
+
+                self.run = False
+                self.draw_game()
+
+                self.draw_end(f"{self.player_turn.name} à gagné la partie !")
+
+
 
     def check_end(self):
         for item in self.matrix_game:
